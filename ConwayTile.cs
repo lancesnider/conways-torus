d68d5cbad26269dd3821316ee594cae1e04395eb
Lance <lance@lancesnider.com>
2016-11-05 21:30:32 -0600

    First Commit

---
+++ ConwayTile.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConwayTile : MonoBehaviour {
+
+	public bool isOn = false;
+	public Material materialOn;
+	public Material materialOff;
+	private Renderer rend; 
+	private float updateTime = 0.3f;
+	private IEnumerator coroutine;
+
+	// Use this for initialization
+	void Start (){
+		rend = GetComponent<Renderer>();
+		rend.enabled = true;
+		OnOrOff ();
+		InvokeRepeating("checkAllColliders", 0, updateTime);
+	}
+		
+
+	void OnOrOff(){
+		if (Random.Range (0, 16) == 1) {
+			changeCellState (true);
+		}
+	}
+
+	void changeCellState (bool newState) {
+		if (!isOn && newState) {
+			rend.sharedMaterial = materialOn;
+		} else if(isOn && !newState) {
+			rend.sharedMaterial = materialOff;
+		}
+		isOn = newState;
+	}
+
+	void checkAllColliders() {
+		gameObject.GetComponent<Collider>().enabled = false;
+		Collider[] hitColliders = Physics.OverlapSphere(transform.position, 1.0f);
+		gameObject.GetComponent<Collider>().enabled = true;
+		int activeNeighbors = 0;
+		foreach(Collider neighbor in hitColliders){
+			ConwayTile otherTile = neighbor.GetComponent<ConwayTile>();
+			if(otherTile.isOn == true) 
+				activeNeighbors++;
+		}
+
+		CheckRules (activeNeighbors);
+
+	}
+
+	void CheckRules(int activeNeighbors){
+		if (isOn == true) {
+			if (activeNeighbors <= 1 || activeNeighbors >= 4) {
+				changeCellState (false);
+			}
+		} else {
+			if (activeNeighbors == 3)
+				changeCellState (true);
+		}
+	}
+
+}
---
+++ CreateTorus.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateTorus : MonoBehaviour {
+
+	public GameObject gameTile;
+	public float TilesPerRing = 108; 
+	public float RingRadius = 30; 
+
+
+	// Use this for initialization
+	void Start () {
+		createRing (gameTile, TilesPerRing, RingRadius);
+	}
+
+	void createRing (GameObject item, float itemCount, float radius) {
+		for (int i = 1; i <= itemCount; i++) {
+			float currentAngle = 2 * Mathf.PI / itemCount * i;
+			float newX = radius * Mathf.Cos (currentAngle);
+			float newY = radius * Mathf.Sin (currentAngle);
+			GameObject newItem = Instantiate(item, new Vector3 (newX, 0, newY), Quaternion.Euler (0, -360/itemCount * i, 0)) as GameObject;
+			newItem.transform.parent = transform;
+		}
+	}
+}
